659711e6188bc67693f7362dc6750d2dbce7df01
mottox2 <y.mottox2@gmail.com>
2023-07-12 15:56:19 +0900

    Show variables types and values

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,39 @@ export default function () {
     }
     if (node.boundVariables) {
       Object.keys(node.boundVariables).forEach(key => {
-        const variables = node.boundVariables ?? [key]
+        console.log({ key, name: node.name, a: node.boundVariables })
+        if (!node.boundVariables) return
+        const _key = key as VariableBindableNodeField // FIXME
+        const vAliasOrAliases = node.boundVariables[_key] // FIXME
+        if (!vAliasOrAliases) return
+        const variables = []
+        if (Array.isArray(vAliasOrAliases)) { // filles, strokes, componentProperties
+          vAliasOrAliases.forEach(alias => {
+            const v = figma.variables.getVariableById(alias.id)
+            // console.log(key, alias.id, v)
+            console.log(v)
+            if (v) {
+              const { name, resolvedType, id, valuesByMode } = v
+              const defaultValue = valuesByMode[Object.keys(valuesByMode)[0]]
+              variables.push({ name, resolvedType, id, defaultValue })
+            }
+          })
+        } else {
+          const v = figma.variables.getVariableById(vAliasOrAliases.id)
+          // console.log(key, vAliasOrAliases.id, v)
+          console.log(v)
+          if (v) {
+            const { name, resolvedType, id, valuesByMode } = v
+            // 本来はvariablesのvariableCollectionIdをもとにcollectionを取得
+            // collectionからmodeを取得し、現在のNodeがどのmodeにあるかを判定しないといけないが面倒なので、最初のvalueを取得することにした
+            const defaultValue = valuesByMode[Object.keys(valuesByMode)[0]]
+            variables.push({ name, resolvedType, id, defaultValue })
+          }
+        }
         map.set(node.name, variables)
+
+        // if (!node.boundVariables) return
+        // map.set(node.name, variables)
       })
     }
   }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,7 +1,7 @@
 import { h } from 'preact'
 import { useState } from 'preact/hooks'
 import { render, useWindowResize } from '@create-figma-plugin/ui'
-import { emit, on } from '@create-figma-plugin/utilities'
+import { convertHexColorToRgbColor, convertRgbColorToHexColor, emit, isValidHexColor, on } from '@create-figma-plugin/utilities'
 
 import { ResizeWindowHandler } from './types'
 
@@ -24,8 +24,16 @@ function Plugin() {
     {Object.keys(data).map(key => {
       const variables = data[key]
       return <div>
-        <h3>{key}</h3>
-        <p>{JSON.stringify(variables || {})}</p>
+        <h3 style={{ fontWeight: 'bold' }}>{key}</h3>
+        {variables.map(variable => {
+          // console.log(variable)
+          const { name, resolvedType, id, defaultValue } = variable
+          const value = typeof defaultValue === 'object' && ("r" in defaultValue) && ("g" in defaultValue) && ("b" in defaultValue) ? "#" + convertRgbColorToHexColor(defaultValue) : defaultValue
+          console.log({ defaultValue, value })
+          return <div>
+            {resolvedType} {name}:{value}
+          </div>
+        })}
       </div>
     })}
   </div>
