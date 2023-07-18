28fb97c83f8ec303849c209e2e7005cc0b528bf9
mottox2 <y.mottox2@gmail.com>
2023-07-19 02:58:41 +0900

    Refactor

--- src/main.ts
+++ src/main.ts
@@ -15,47 +15,62 @@ export default function () {
     }
   )
 
-  const collectVariables = (node: SceneNode, map: Map<string, any>) => {
-    if ("children" in node) node.children.forEach(child => collectVariables(child, map));
-
-    // NOTE: 現状Set Variables, Conditionalのactionは取得できない
-    if ("reactions" in node)
-      node.reactions.forEach(reaction => console.log('[VariablesViewer]', reaction))
-
-    // console.log('[VariablesViewer]', node.boundVariables)
-    if (node.boundVariables) {
-      const variables: any = [];
-      const processVariable = (alias: VariableAlias) => {
-        const variable = figma.variables.getVariableById(alias.id);
-        if (!variable) return;
-        // console.log(alias, alias.id, v);
-        const { name, resolvedType, id, valuesByMode } = variable;
-        const defaultValue = valuesByMode[Object.keys(valuesByMode)[0]];
-        variables.push({ name, resolvedType, id, defaultValue });
-      };
-      const { fills, strokes, componentProperties = {}, ...otherVariables } = node.boundVariables
-      fills?.forEach(processVariable)
-      strokes?.forEach(processVariable)
-      Object.entries(componentProperties).forEach(([_, alias]) => processVariable(alias))
-      Object.entries(otherVariables).forEach(([_, alias]) => processVariable(alias))
-      if (variables.length > 0) map.set(node.name, variables);
-    }
-  };
-
   figma.on(
     'selectionchange',
     async () => {
+      const variables: Record<string, any> = {}
+      const nodes: Record<string, any> = {}
+      const collectVariables = (node: SceneNode, map: Map<string, any>) => {
+        if ("children" in node) node.children.forEach(child => collectVariables(child, map));
+
+        // console.log('[VariablesViewer]', node.boundVariables)
+        if (node.boundVariables) {
+          let useNode = false
+          const processVariable = (alias: VariableAlias) => {
+            console.log(alias.id)
+            const variable = figma.variables.getVariableById(alias.id);
+            if (!variable) return;
+            // console.log(alias, alias.id, v);
+            const { name, resolvedType, id, valuesByMode } = variable;
+            const defaultValue = valuesByMode[Object.keys(valuesByMode)[0]];
+            variables[id] = { name, resolvedType, id, defaultValue }
+            useNode = true
+          };
+          const { fills, strokes, componentProperties = {}, ...otherVariables } = node.boundVariables
+          fills?.forEach(processVariable)
+          strokes?.forEach(processVariable)
+          Object.entries(componentProperties).forEach(([_, alias]) => processVariable(alias))
+          Object.entries(otherVariables).forEach(([_, alias]) => processVariable(alias))
+          if (!useNode) return
+          console.log(node)
+          nodes[node.id] = {
+            name: node.name,
+            type: node.type,
+            variables: node.boundVariables,
+            relatedActions: []
+          }
+        }
+
+        // NOTE: 現状Set Variables, Conditionalのactionは取得できない
+        // actionが取得できないreactionはこれらのアクションである可能性がある
+        // TODO: 無料プランではここの処理をスキップしたい
+        if (nodes[node.id] && "reactions" in node)
+          node.reactions.forEach(reaction => {
+            console.log('[VariablesViewer]', reaction)
+            if (reaction.action) return
+            nodes[node.id].relatedActions.push(reaction.trigger?.type)
+          })
+      };
+
       const selection = figma.currentPage.selection
       console.log(selection)
-      const variables = new Map<string, any>()
+      // const variables = new Map<string, any>()
       selection.forEach(node => {
-        collectVariables(node, variables)
+        collectVariables(node, new Map())
       })
-      const result = Object.fromEntries(variables)
+      // const result = Object.fromEntries(variables)
 
-      emit('COLLECT_VARIABLES', {
-        data: result
-      });
+      emit('COLLECT_VARIABLES', { variables, nodes });
     }
   );
 
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,14 +1,26 @@
-import { FunctionComponent, h } from 'preact'
+import { FunctionComponent, h, Fragment } from 'preact'
 import { useState } from 'preact/hooks'
 import { render, useWindowResize } from '@create-figma-plugin/ui'
 import { convertHexColorToRgbColor, convertRgbColorToHexColor, emit, isValidHexColor, on } from '@create-figma-plugin/utilities'
 
 import { ResizeWindowHandler } from './types'
 
-const Variable: FunctionComponent = ({ children }) => {
-  return <span style={{ fontSize: 11, padding: '1px 5px', border: 'var(--figma-color-border)1px solid', backgroundColor: 'var(--figma-color-bg-secondary)' }}>
+const VariableText: FunctionComponent<{
+  title: string
+}> = ({ children, title }) => {
+  return <span title={title} style={{ fontSize: 11, padding: '1px 5px', border: 'var(--figma-color-border)1px solid', backgroundColor: 'var(--figma-color-bg-secondary)' }}>
     {children}
   </span>
+}
+
+const Variable = ({ label, variable }: any) => {
+  const { name, resolvedType, id, defaultValue } = variable
+  const isColor = resolvedType === 'COLOR' && typeof defaultValue === 'object' && ("r" in defaultValue) && ("g" in defaultValue) && ("b" in defaultValue)
+  const value = isColor ? "#" + convertRgbColorToHexColor(defaultValue) : defaultValue
+  return <div>{label}: {isColor && <span style={{
+    width: 16, height: 16, display: 'inline-block', verticalAlign: 'middle', backgroundColor: value
+  }}>
+  </span>} <VariableText title={value}>{name}</VariableText>:{value}</div>
 }
 
 function Plugin() {
@@ -22,22 +34,45 @@ function Plugin() {
     minWidth: 120,
     resizeBehaviorOnDoubleClick: 'minimize'
   })
-  const [data, setData] = useState({})
-  on('COLLECT_VARIABLES', ({ data }) => {
-    setData(data)
+  const [data, setData] = useState<{
+    nodes: Record<string, any>
+    variables: Record<string, any>
+  }>({
+    nodes: {},
+    variables: {}
   })
+  on('COLLECT_VARIABLES', (data) => {
+    setData(data)
+    console.log(data)
+  })
+  const { variables, nodes } = data
+
   return <div>
-    {Object.entries(data).map(([key, variables]) => {
+    {Object.values(nodes).map(node => {
+      console.log({ node })
+      const { id, variables: aliases, name, relatedActions } = node as {
+        id: string
+        name: string
+        variables: SceneNodeMixin['boundVariables'],
+        relatedActions: string[]
+      }
+      const { fills, strokes, componentProperties = {}, ...otherVariables } = aliases || {}
       return <div>
-        <h3 style={{ fontWeight: 'bold' }}>{key}</h3>
-        {(variables as any[]).map(variable => {
-          // console.log(variable)
-          const { name, resolvedType, id, defaultValue } = variable
-          const value = typeof defaultValue === 'object' && ("r" in defaultValue) && ("g" in defaultValue) && ("b" in defaultValue) ? "#" + convertRgbColorToHexColor(defaultValue) : defaultValue
-          // console.log({ defaultValue, value })
-          return <div>
-            {resolvedType} <Variable>{name}</Variable>:{value}
-          </div>
+        <h3 style={{ fontWeight: 'bold' }}>{name}</h3>
+        {fills?.map(fill => {
+          return <Variable label="fill" variable={variables[fill.id]} />
+        })}
+        {strokes?.map(stroke => {
+          return <Variable label="stroke" variable={variables[stroke.id]} />
+        })}
+        {Object.entries(componentProperties).map(([property, alias]) => {
+          return <Variable label={property} variable={variables[alias.id]} />
+        })}
+        {Object.entries(otherVariables).map(([property, alias]) => {
+          return <Variable label={property} variable={variables[alias.id]} />
+        })}
+        {relatedActions.map(trigger => {
+          return <div>{trigger} may have variable related actions</div>
         })}
       </div>
     })}
